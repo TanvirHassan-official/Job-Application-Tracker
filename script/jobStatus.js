fa52009b2efe3a8814f37f4fa1db9318b2fd0646
TanvirHassan-official <tanvirhassan0006@gmail.com>
2026-03-05 12:50:45 +0600

    design and functionality part complete

--- script/jobStatus.js
+++ script/jobStatus.js
@@ -5,7 +5,7 @@ const dashInterview = document.querySelector('#dashboard .card:nth-child(2) p');
 const dashRejected = document.querySelector('#dashboard .card:nth-child(3) p');
 
 function countUpdate(){
-    const totalJobs = document.querySelectorAll('#job-container .card').length;
+    const totalJobs = document.querySelectorAll('#job-container .job-card').length;
     const interviewJobs = document.querySelectorAll('#interview-section .job-card').length;
     const rejectedJobs = document.querySelectorAll('#rejected-section .job-card').length;
 
@@ -23,7 +23,7 @@ function countUpdate(){
     const rejectedSection = document.getElementById('rejected-section');
 
 function interview(button) {
-    const card = button.closest('.card');
+    const card = button.closest('.job-card');
 
     const status = card.querySelector('.card-status');
     if(status.innerText === 'NOT APPLIED'){
@@ -32,72 +32,101 @@ function interview(button) {
         const i = document.querySelector('#interview-section .no-card');
         i.classList.add('hidden');
         
-        const clone = card.cloneNode(true);
-        clone.card = card + "-interview";
-        clone.classList.add('job-card');
+        interviewSection.appendChild(card);
 
 
-        interviewSection.appendChild(clone);
-
-        countUpdate();
     }
     else if(status.innerText === 'REJECTED'){
         status.innerText = 'INTERVIEW';
 
         const i = document.querySelector('#interview-section .no-card');
         i.classList.add('hidden');
-
-        const c = card.id;
-
-        const s = rejectedSection.querySelector('#'+c);
         
-        interviewSection.appendChild(s);
-        countUpdate();
+        interviewSection.appendChild(card);
 
-        if(dashRejected.innerText === '0'){
+
+
+      
+        
+    }
+    countUpdate();
+
+    if(dashRejected.innerText === '0'){
             const j = document.querySelector('#rejected-section .no-card');
             j.classList.remove('hidden');
-        }
+        }  
 
-    }
+    if(dashTotal.innerText === '0'){
+            const j = document.querySelector('.no-card');
+            j.classList.remove('hidden');
+        } 
 }
 
 
 function rejected(button) {
-    const card = button.closest('.card');
+    const card = button.closest('.job-card');
     const status = card.querySelector('.card-status');
     if(status.innerText === 'NOT APPLIED'){
         status.innerText = 'REJECTED';
 
         const i = document.querySelector('#rejected-section .no-card');
         i.classList.add('hidden');
+
+        rejectedSection.appendChild(card);
+
         
-        const clone = card.cloneNode(true);
-        clone.card = card + "-rejected";
-        clone.classList.add('job-card');
-
-        rejectedSection.appendChild(clone);
-
-        countUpdate();
     }
-    else if(status === "INTERVIEW"){
+    else if(status.innerText === "INTERVIEW"){
         status.innerText = 'REJECTED';
 
         const i = document.querySelector('#rejected-section .no-card');
         i.classList.add('hidden');
-
-        const c = card.id;
-
-        const s = interviewSection.querySelector('#'+c);
         
-        rejectedSection.appendChild(s);
-        countUpdate();
-
-        if(dashRejected.innerText === '0'){
-            const j = document.querySelector('#interview-section .no-card');
-            j.classList.remove('hidden');
-        }
+        rejectedSection.appendChild(card);
 
     }
+    countUpdate();
+
+    if(dashInterview.innerText === '0'){
+        const j = document.querySelector('#interview-section .no-card');
+        j.classList.remove('hidden');
+    }
+
+    if(dashTotal.innerText === '0'){
+            const j = document.querySelector('.no-card');
+            j.classList.remove('hidden');
+        }
+}
+
+
+
+    // if(dashRejected.innerText === '0'){
+    //         const j = document.querySelector('#rejected-section .no-card');
+    //         j.classList.remove('hidden');
+    //     }
+
+
+
+function deleteCard(button) {
+    const card = button.closest('.job-card');
+    const parent = card.parentNode;
+    parent.removeChild(card);
     
+    countUpdate();
+    
+
+    if(dashInterview.innerText === '0'){
+        const j = document.querySelector('#interview-section .no-card');
+        j.classList.remove('hidden');
+    }
+    
+    if(dashRejected.innerText === '0'){
+            const j = document.querySelector('#rejected-section .no-card');
+            j.classList.remove('hidden');
+        } 
+
+    if(dashTotal.innerText === '0'){
+            const j = document.querySelector('.no-card');
+            j.classList.remove('hidden');
+        }
 }
